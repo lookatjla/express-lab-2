2b8b777018fb0df43343775620b98742c8d264c6
Jayla Young <jaylayoung1@gmail.com>
2022-11-04 16:41:29 -0400

    greeting express application created

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* const express = require('express');
+const express = require('express');
 const app = express();
 
 app.get('/ash', (request, response) => {
@@ -6,19 +6,3 @@ app.get('/ash', (request, response) => {
 });
 
 app.listen(4000);
-*/
-
-
-const express = require('express');
-const app = express();
-let answer = Math.floor(200 * .18);
-
-const port = 4000;
-
-app.get('/tip/:200/:18', (request, response) => {
-    response.send('The tip is $' + answer + '.');
-});
-
-app.listen(port, () => {
-    console.log('Express is listening on port: ', port);
-});
